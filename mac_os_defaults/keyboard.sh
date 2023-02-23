9044b7dded697045c8960d77e0a148da757d02a6
Burhan Khanzada <18241005+burhankhanzada@users.noreply.github.com>
2023-02-23 11:52:08 +0500

    update keyrepeat

--- mac_os_defaults/keyboard.sh
+++ mac_os_defaults/keyboard.sh
@@ -1,9 +1,11 @@
+#!/usr/bin/env bash
+
 # Disable press-and-hold for keys in favor of key repeat
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
 # Set a blazingly fast keyboard repeat rate
-defaults write NSGlobalDomain KeyRepeat -int 1
-defaults write NSGlobalDomain InitialKeyRepeat -int 10
+defaults write NSGlobalDomain KeyRepeat -int 3
+defaults write NSGlobalDomain InitialKeyRepeat -int 12
 
 # Disable automatic capitalization as it’s annoying when typing code
 defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
