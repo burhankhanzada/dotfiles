13f578a0eef68ed5a80d5483333bd945be2488bb
Burhan Khanzada <burhankhanzada199888@gmail.com>
2023-04-13 01:17:13 +0500

    remev explicit set android sdk path fro flutter coz flutter will use deafult path but that will be symlink

--- after_brew/flutter/install.sh
+++ after_brew/flutter/install.sh
@@ -8,9 +8,3 @@ brew install fvm
 fvm install stable
 
 dart pub global activate fvm
-
-# export GOOGLE_PATH="$DEVELOPMENT/Google"
-# export ANDROID_PATH="$GOOGLE_PATH/Android"
-# export FLUTTER_PATH="$GOOGLE_PATH/Flutter"
-
-# fvm flutter config --android-sdk $ANDROID_PATH/sdk
