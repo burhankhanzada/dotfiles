11bec83d9065748ee626d9c443ae333891f332ae
Burhan Khanzada <burhankhanzada199888@gmail.com>
2023-04-24 14:41:37 +0500

    disable auto hide dock

--- mac_os_defaults/dock.sh
+++ mac_os_defaults/dock.sh
@@ -1,27 +1,24 @@
 #!/usr/bin/env bash
 
-echo "1 - Autohide when the mouse is out"
-defaults write com.apple.dock autohide -bool true
-
-echo "2 - Set tile icon size to 25"
+echo "1 - Set tile icon size to 25"
 defaults write com.apple.dock tilesize -int 25
 
-echo "3 - Set large icon size to 50"
+echo "2 - Set large icon size to 50"
 defaults write com.apple.dock largesize -int 50
 
-echo "4 - Disable automatically rearrange spaces based on use"
+echo "3 - Disable automatically rearrange spaces based on use"
 defaults write com.apple.dock mru-spaces -bool false
 
-echo "5 - Enable magnification"
+echo "4 - Enable magnification"
 defaults write com.apple.dock magnification -bool true
 
-echo "6 - Disable recents apps"
+echo "5 - Disable recents apps"
 defaults write com.apple.dock show-recents -bool false
 
-echo "7 - Enable minimize apps into icon"
+echo "6 - Enable minimize apps into icon"
 defaults write com.apple.dock minimize-to-application -bool true
 
-echo "8 - Enable minimize apps into icon"
+echo "7 - Enable minimize apps into icon"
 defaults write com.apple.Dock ResetLaunchPad -bool true
 
 killall Dock
