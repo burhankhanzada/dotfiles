4e5583b994fc0c7180b3efdb7775138258cf6982
Burhan Khanzada <18241005+burhankhanzada@users.noreply.github.com>
2023-05-02 01:41:30 +0500

    remove sudo

--- packages/yabai/install.sh
+++ packages/yabai/install.sh
@@ -3,7 +3,7 @@
 brew install koekeishiya/formulae/skhd
 brew install koekeishiya/formulae/yabai
 
-sudo brew services start skhd
-sudo brew services start yabai
+brew services start skhd
+brew services start yabai
 
 export YABAI_PATH=$PACKAGES_PATH/yabai
