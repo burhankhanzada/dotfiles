1d4078646108e5ea1fe5dd9bd331c5fb8862ee6a
Burhan Khanzada <burhankhanzada199888@gmail.com>
2023-05-18 22:44:38 +0500

    fix flutter path

--- packages/flutter/after_links.sh
+++ packages/flutter/after_links.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
+sdk="$HOME/fvm/default/bin"
 pub_cache="$HOME/.pub-cache/bin"
 
+echo "path+=$sdk" >>~/.zshrc
 echo "path+=$pub_cache" >>~/.zshrc
 echo "export PATH" >>~/.zshrc
