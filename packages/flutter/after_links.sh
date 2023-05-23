80dfd02b67f3b3b368746457af10edcaac7acaab
Burhan Khanzada <burhankhanzada199888@gmail.com>
2023-05-23 23:21:13 +0500

    fix flutter

--- packages/flutter/after_links.sh
+++ packages/flutter/after_links.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
 
+dart pub global activate fvm
+dart pub global activate flutter_gen
+
 fvm install stable
+
+pub_cache="$HOME/.pub-cache/bin"
 
 fvm_path=$HOME/fvm
 
-pub_cache="$HOME/.pub-cache/bin"
-sdk_default=$fvm_path/default/bin
-sdk_stable=$fvm_path/versions/stable/bin
+sdk_stable=$fvm_path/versions/stable
+sdk_stable_bin=$sdk_stable/bin
 
-echo "path+=$pub_cache" >>$HOME/.zshrc
 echo "path+=$sdk_stable" >>$HOME/.zshrc
-echo "path+=$sdk_default" >>$HOME/.zshrc
+echo "path+=$sdk_stable_bin" >>$HOME/.zshrc
+echo "path+=$pub_cache" >>$HOME/.zshrc
 echo "export PATH" >>$HOME/.zshrc
 
 source $HOME/.zshrc
--- packages/flutter/install.sh
+++ packages/flutter/install.sh
@@ -2,12 +2,9 @@
 
 # https://github.com/fluttertools/fvm
 
-sudo softwareupdate --install-rosetta --agree-to-license
-
 brew tap leoafarias/fvm
 brew install fvm
 
-dart pub global activate fvm
-dart pub global activate flutter_gen
+
 
 export FLUTTER_PATH=$DEVELOPMENT/Google/Flutter
