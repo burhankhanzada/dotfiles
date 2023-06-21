2c3cb94dd83e4df2e6fc61e341f5d0fa019adfbe
Burhan Khanzada <burhankhanzada199888@gmail.com>
2023-06-22 00:09:29 +0500

    added homebrew folder back remvoed accidently

---
+++ brew/setup.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+echo.Blue "Running Homebrew setup"
+
+cd $DOTFILES/brew
+
+if [ -f "install.sh" ]; then
+    echo.Yellow "Installing from $0"
+    chmod +x install.sh
+    source install.sh
+fi
+
+if [ -f "links.prop" ]; then
+    echo.Yellow "Linking from $0"
+    link_files links.prop
+fi
