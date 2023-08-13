5fdfd611d2a87e957c07caafe677c41319d5ca65
Burhan Khanzada <burhankhanzada199888@gmail.com>
2023-08-13 12:47:30 +0500

    Added audacity

---
+++ packages/audacity/install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+brew install audacity
+
+dockutil --add "/Applications/Audacity.app"
---
+++ packages/edge/install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+brew install microsoft-edge
+
+dockutil --add "/Applications/Microsft Edge.app"
