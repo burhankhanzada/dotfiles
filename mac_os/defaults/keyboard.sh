8136449333656426f14ff30d5d3876f7be4b4ebf
Burhan Khanzada <burhankhanzada199888@gmail.com>
2023-10-29 09:33:22 +0500

    comment keybaord related defaults to get press and hold behavoir for german letters

--- mac_os/defaults/keyboard.sh
+++ mac_os/defaults/keyboard.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-echo.Green "1 - Set a fast keyboard repeat rate"
-defaults write -g KeyRepeat -int 3
-defaults write -g InitialKeyRepeat -int 12
+# echo.Green "1 - Set a fast keyboard repeat rate"
+# defaults write -g KeyRepeat -int 3
+# defaults write -g InitialKeyRepeat -int 12
 
-echo.Green "2 - Disable press-and-hold for keys in favor of key repeat"
-defaults write -g ApplePressAndHoldEnabled -bool false
+# echo.Green "2 - Disable press-and-hold for keys in favor of key repeat"
+# defaults write -g ApplePressAndHoldEnabled -bool false
 
-echo.Green "3 - Disable automatic capitalization"
-defaults write -g NSAutomaticCapitalizationEnabled -bool false
+# echo.Green "3 - Disable automatic capitalization"
+# defaults write -g NSAutomaticCapitalizationEnabled -bool false
