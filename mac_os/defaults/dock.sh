f40db11533073c0e642c13e9bfae6bf2341dff83
Burhan Khanzada <burhankhanzada199888@gmail.com>
2023-12-26 14:48:27 +0500

    Update dock deualts

--- mac_os/defaults/dock.sh
+++ mac_os/defaults/dock.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo.Green "1 - Set tile icon size to 25"
-defaults write com.apple.dock tilesize -int 25
+defaults write com.apple.dock tilesize -int 35
 
 echo.Green "2 - Set large icon size to 50"
 defaults write com.apple.dock largesize -int 50
@@ -12,7 +12,10 @@ defaults write com.apple.dock magnification -bool true
 echo.Green "4 - Disable recents apps"
 defaults write com.apple.dock show-recents -bool false
 
-echo.Green "5 - Enable minimize apps into icon"
+echo.Green "5 - Set position to left"
+defaults write com.apple.dock "orientation" -string left
+
+echo.Green "6 - Enable minimize apps into icon"
 defaults write com.apple.dock minimize-to-application -bool true
 
 killall Dock
