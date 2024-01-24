4d233b22c3e40949e00151a5ab24ec98ab800ac1
Burhan Khanzada <burhankhanzada199888@gmail.com>
2024-01-24 08:21:39 +0500

    Added launcpad row and column size

--- mac_os/defaults/launchpad.sh
+++ mac_os/defaults/launchpad.sh
@@ -3,4 +3,8 @@
 echo.Green "1 - Reset launchpad"
 defaults write com.apple.Dock ResetLaunchPad -bool true
 
+echo.Green "2 - Change row and column size"
+defaults write com.apple.dock springboard-rows -int 6
+defaults write com.apple.dock springboard-columns -int 8
+
 killall Dock
