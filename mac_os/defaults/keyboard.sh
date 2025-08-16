2b702fc721166fe011190912e94357d46cb2c8f0
burhankhanzada <burhankhanzada199888@gmail.com>
2025-08-16 19:47:22 +0200

    Enables keyboard tweaks and input source shortcuts

--- mac_os/defaults/keyboard.sh
+++ mac_os/defaults/keyboard.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
 
-# echo.Green "1 - Set a fast keyboard repeat rate"
-# defaults write -g KeyRepeat -int 3
-# defaults write -g InitialKeyRepeat -int 12
+echo.Green "1 - Set a fast keyboard repeat rate"
+defaults write -g KeyRepeat -int 3
+defaults write -g InitialKeyRepeat -int 12
 
-# echo.Green "2 - Disable press-and-hold for keys in favor of key repeat"
-# defaults write -g ApplePressAndHoldEnabled -bool false
+echo.Green "2 - Disable press-and-hold for keys in favor of key repeat"
+defaults write -g ApplePressAndHoldEnabled -bool false
 
-# echo.Green "3 - Disable automatic capitalization"
-# defaults write -g NSAutomaticCapitalizationEnabled -bool false
+echo.Green "3 - Disable automatic capitalization"
+defaults write -g NSAutomaticCapitalizationEnabled -bool false
+
+echo.Green "4 - Disable previous input source shorcut"
+defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 60 '<dict><key>enabled</key><false/></dict>'
+
+echo.Green "5 - Disable next input source shorcut" 
+defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 61 '<dict><key>enabled</key><false/></dict>'
+
+echo.Green "5 - Make fn to change input source" 
+deffaults write com.apple.HIToolbox AppleFnUsageType --int 1
