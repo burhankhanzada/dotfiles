d0adeb3c68bcd336ebd41b99e46df5b7aafbed37
burhankhanzada <burhankhanzada199888@gmail.com>
2025-09-21 12:27:37 +0200

    update rust

--- packages/ruby/links.sh
+++ packages/ruby/links.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 
-echo "" >>$HOME/.zshrc
-echo "export PATH=$HOME/.gem/bin:$PATH" >>$HOME/.zshrc
-
-echo 'source $HOMEBREW_PREFIX/opt/chruby/share/chruby/chruby.sh' >>$HOME/.zshrc
-echo 'source $HOMEBREW_PREFIX/opt/chruby/share/chruby/auto.sh' >>$HOME/.zshrc
-echo "chruby ruby-$RUBY_VERSION" >>$HOME/.zshrc
-
 ruby=$DEVELOPMENT/Ruby
 
 symlink $ruby/src $HOME/src
